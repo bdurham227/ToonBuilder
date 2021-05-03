2d82ea13b4e259ca8047132580b6702fcba40978
bdurham227 <bdurham227@gmail.com>
2021-05-03 19:58:54 -0400

    finished account routes and pushing those changes up

--- routes/api/accountRoutes.js
+++ routes/api/accountRoutes.js
@@ -3,14 +3,43 @@ const { Account, User, Character } = require('../../models');
 
 //get all accounts
 router.get('/', async (req, res) => {
-    try {} catch (err) {
+    try {
+        const accountData = await Account.findAll({
+            attributes: ['id', 'account_name', 'user_id'],
+            include: [
+                {
+                    model: Character,
+                    attributes: ["id", "character_name", "character_class", "character_level", "user_id", "account_id"],
+                },
+            ],
+        });
+        res.status(200).json(accountData);
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
 //get one account
 router.get('/:id', async (req, res) => {
-    try {} catch (err) {
+    try {
+    const accountData = await Account.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id', 'account_name', 'user_id'],
+        include: [
+            {
+                model: Character,
+                attributes:  ["id", "character_name", "character_class", "character_level", "user_id", "account_id"],
+            },
+        ],
+    });
+    if (!accountData) {
+        res.status(404).json({ message: `No User with the Id ${req.params.id} found!` });
+        return;
+    }
+    res.status(200).json(accountData);
+    } catch (err) {
         res.status(500).json(err);
     }
 })
@@ -18,21 +47,39 @@ router.get('/:id', async (req, res) => {
 
 //create an account
 router.post('/', async (req, res) => {
-    try {} catch (err) {
+    try {
+        const accountData = await Account.create({
+            account_name: req.body.account_name,
+        });
+        res.status(200).json(accountData);
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
 //update an account
-router.put('/:id', async (req, res) => {
-    try {} catch (err) {
-        res.status(500).json(err);
-    }
-})
+// router.put('/:id', async (req, res) => {
+//     try {
+//         const accountData = awa
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
 
 //delete an account
 router.delete('/:id', async (req, res) => {
-    try {} catch (err) {
+    try {
+        const accountData = await Account.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if(!accountData) {
+            res.status(404).json({ message: `No User with the Id ${req.params.id} found!` });
+            return;
+        }
+        res.status(200).json(accountData);
+    } catch (err) {
         res.status(500).json(err);
     }
 })
