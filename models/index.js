9249f909b2a3c0ae6b865e18e54d6433dc571047
bdurham227 <bdurham227@gmail.com>
2021-05-03 19:26:27 -0400

    had to drop database to re-seed it- pushing working associations in seed/index.js

--- models/index.js
+++ models/index.js
@@ -5,17 +5,38 @@ const Character = require('./Character');
 
 //set up assocations
 
-
+User.hasMany(Account,{
+    onDelete: 'cascade',
+});
+Account.belongsTo(User,{
+    onDelete: 'cascade',
+})
 
 Character.belongsTo(Account,{
     foreignKey: 'account_id',
+    onDelete: 'cascade',
 });
 
 Account.hasMany(Character,{
     foreignKey: 'account_id',
+    onDelete: 'cascade',
 });
 
+User.belongsToMany(Account, {
+    through: {
+        model: Character,
+    },
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+});
 
+Account.belongsToMany(User, {
+    through: {
+        model: Character,
+    },
+    foreignKey: 'account_id',
+    onDelete: 'cascade',
+});
 
 
 
